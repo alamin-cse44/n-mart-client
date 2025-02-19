6426ed05810a4f88fdb2b14a8dc3de776e38e354
alamin-cse44 <alamincse44@gmail.com>
2025-02-19 09:37:45 +0600

    Chore: dashboard

---
+++ src/app/(WithDashboardLayout)/admin/page.tsx
@@ -0,0 +1,9 @@
+const AdminPage = () => {
+    return (
+        <div>
+            <h1>Admin Home Page!</h1>
+        </div>
+    );
+};
+
+export default AdminPage;
---
+++ src/app/(WithDashboardLayout)/user/shop/category/page.tsx
@@ -0,0 +1,9 @@
+const ProductCategoryPage = () => {
+    return (
+        <div>
+            <h1>This is shop category page!</h1>
+        </div>
+    );
+};
+
+export default ProductCategoryPage;
