981e7525920ffccf9152d3bc5a59fbf743017477
alamin-cse44 <alamincse44@gmail.com>
2025-02-23 01:22:51 +0600

    Chore: category

--- src/app/(WithDashboardLayout)/user/shop/category/page.tsx
+++ src/app/(WithDashboardLayout)/user/shop/category/page.tsx
@@ -1,9 +1,13 @@
-const ProductCategoryPage = () => {
-    return (
-        <div>
-            <h1>This is shop category page!</h1>
-        </div>
-    );
+import ManageCategories from "@/components/modules/shop/category";
+import { getAllCategories } from "@/services/Category";
+
+const ProductCategoryPage = async () => {
+  const { data, meta } = await getAllCategories();
+  return (
+    <div>
+      <ManageCategories categories={data} />
+    </div>
+  );
 };
 
 export default ProductCategoryPage;
